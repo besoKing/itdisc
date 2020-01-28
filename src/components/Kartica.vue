37375708957796c4c64d9f99fec8346342beacc7
bmacan <58048622+bmacan@users.noreply.github.com>
2020-01-28 14:55:19 +0100

    kjhk

--- src/components/Kartica.vue
+++ src/components/Kartica.vue
@@ -1,14 +1,24 @@
 <template>
     <div class="card">
-  <h5 class="card-header">Featured</h5>
+  <h5 class="card-header"> Links </h5><div class="img"><p><img src="@/assets/links.png"></p></div>
+  
   <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
+    
     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="#" class="btn btn-primary">Idi na trgovinu</a>
   </div>
 </div>
 </template>
 
-<style lang=scss scope>
+<style >
+.card{  
+    margin-top: 50px;
+    width: 350px;
+    height: 350px;
+};
+.img{
+  width: 100px;
+  height: 50px;
+}
 
 </style>
